a4b18ff0abe6302f48fad04f075d09695756f3ed
renalsugiana <a22100107@mhs.stmik-sumedang.ac.id>
2024-11-08 02:23:04 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ import routes from './routes.js';
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
